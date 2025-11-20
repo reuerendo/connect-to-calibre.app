4517893661c8f4492c65c04bf1c7d5241d9665c2
Oleh Tiuriakov <152391484+reuerendo@users.noreply.github.com>
2025-11-20 13:37:31 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,9 +26,10 @@ static const char *DEFAULT_READ_DATE_COLUMN = "#read_date";
 static const char *DEFAULT_FAVORITE_COLUMN = "#favorite";
 static const char *DEFAULT_INPUT_FOLDER = "/mnt/ext1/Books";
 
+static const char *onoff[] = { "Off", "On", NULL };
+
 // Config editor structure
 static iconfigedit configItems[] = {
-	static const char *onoff[] = { "Off", "On", NULL };
 	{
 		CFG_CHOICE,
 		NULL,
