c10b45d177479f08796d84579d21bf39078b4545
Oleh Tiuriakov <152391484+reuerendo@users.noreply.github.com>
2025-12-30 12:30:30 +0100

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -8,6 +8,7 @@ set(TOOLCHAIN_PATH "${CMAKE_SOURCE_DIR}/SDK/SDK_6.3.0/SDK-B288" CACHE PATH "Path
 
 set(CMAKE_C_COMPILER ${TOOLCHAIN_PATH}/usr/bin/arm-obreey-linux-gnueabi-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PATH}/usr/bin/arm-obreey-linux-gnueabi-g++)
+set(CMAKE_STRIP ${TOOLCHAIN_PATH}/usr/bin/arm-obreey-linux-gnueabi-strip)
 
 # Where is the target environment
 set(CMAKE_FIND_ROOT_PATH ${TOOLCHAIN_PATH}/usr/arm-obreey-linux-gnueabi/sysroot)
@@ -19,6 +20,16 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# Compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+# Prefer static libraries
+set(CMAKE_FIND_LIBRARY_SUFFIXES .a .so)
+
+# Compiler flags for static linking
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -static-libgcc")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -static-libgcc -static-libstdc++")
+
+# Linker flags for static linking
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++ -Wl,--as-needed")
+
+# Optional: Full static linking (uncomment if needed)
+# set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static")
+# set(BUILD_SHARED_LIBS OFF)
